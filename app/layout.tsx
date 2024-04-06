c870287dd619ada4adfe7729ef894e0ae69eb60a
Meliora Ho <nho10@fordham.edu>
2024-04-06 18:44:29 +0000

    Added a ChakraUI provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
+          <Providers>
           {children}
+          </Providers>
         </main>
       </body>
     </html>
