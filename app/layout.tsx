189b67f1ef6546f16ae11f3877ac9ee7de9f00e9
Meliora Ho <nho10@fordham.edu>
2024-04-09 23:33:33 +0000

    Set metadata and replaced tab with modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Galiscope",
+  description: "An AI-powered tool for amateur and neurodivergent researchers",
 };
 
 export default function RootLayout({
