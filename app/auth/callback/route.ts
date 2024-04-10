3451512440703770d9574fa9476df98f5e142d29
nhanaa <nhanpro7d@gmail.com>
2024-04-09 22:49:16 -0500

    Fix formatting and change gitignore

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 import { type CookieOptions, createServerClient } from '@supabase/ssr'
 
 export async function GET(request: Request) {
-    const requestUrl = new URL(request.url)
+  const requestUrl = new URL(request.url)
   const { searchParams, origin } = requestUrl
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
