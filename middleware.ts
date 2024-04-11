54b525cc815179f507d415f8fa255eddeab60cbc
nhanaa <nhanpro7d@gmail.com>
2024-04-11 17:34:59 -0500

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     "/protected:path*",
     "/api:path*",
   ],
