1da5e9006983f05726118751dce3150ec4bcb488
Meliora Ho <nho10@fordham.edu>
2024-04-12 00:29:21 +0000

    Changed hover effect

--- components/ReadingCard.tsx
+++ components/ReadingCard.tsx
@@ -12,23 +12,19 @@ export const ReadingCard: React.FC<ReadingCardProps> = ({
   lastUpdatedTime,
 }) => {
   return (
-    <Box
-      overflow="hidden"
-      borderWidth="1px"
-      borderRadius="lg"
-      maxWidth={225}
-      
-    >
-      <Image src={blurredImage.src} width="full" display="fill" />
-        <div className="p-2 space-y-1 border-t border-gray-200 bg-white hover:bg-gray-50 cursor-pointer">
-        <Heading isTruncated color="gray.600" size="xs">
-          {paperTitle}
-        </Heading>
-        <div className="flex flex-row justify-between text-xs text-gray-500">
-          <Text isTruncated>{folder}</Text>
-          <Text isTruncated>{lastUpdatedTime}</Text>
+    <div className="hover:mix-blend-darken">
+      <Box overflow="hidden" borderWidth="1px" borderRadius="lg" maxWidth={225}>
+        <Image src={blurredImage.src} width="full" display="fill" />
+        <div className="p-2 space-y-1 border-t border-gray-200 bg-white cursor-pointer">
+          <Heading isTruncated color="gray.600" size="xs">
+            {paperTitle}
+          </Heading>
+          <div className="flex flex-row justify-between text-xs text-gray-500">
+            <Text isTruncated>{folder}</Text>
+            <Text isTruncated>{lastUpdatedTime}</Text>
+          </div>
         </div>
-        </div>
-    </Box>
+      </Box>
+    </div>
   );
 };
