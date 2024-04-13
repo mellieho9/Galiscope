546f7e7ae8868b01a52b37f65bdba5d0c0a271bf
nhanaa <nhanpro7d@gmail.com>
2024-04-13 03:46:55 -0500

    Connect avatar with backend part 3

--- components/dashboard/Sidebar/UserProfile/index.tsx
+++ components/dashboard/Sidebar/UserProfile/index.tsx
@@ -9,7 +9,7 @@ export function UserProfile() {
   const { data: user } = userData ?? {};
 
   return (
-    <div className="p-4 border-b border-gray-200 flex flex-row justify-between items-center">
+    <div className="p-4 border-b border-gray-200">
       {user ? (
         <div className="w-full flex flex-row justify-between items-center">
           <AvatarAction user={user} />
