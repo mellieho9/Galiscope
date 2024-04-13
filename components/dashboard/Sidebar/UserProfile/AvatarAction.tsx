32ac900f2c9c69b8cdeec87189a22a2edfe2f1ca
nhanaa <nhanpro7d@gmail.com>
2024-04-13 03:27:06 -0500

    Connect avatar with backend

--- components/dashboard/Sidebar/UserProfile/AvatarAction.tsx
+++ components/dashboard/Sidebar/UserProfile/AvatarAction.tsx
@@ -1,25 +1,27 @@
+import { User } from '@/types/user.types';
 import {
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuList,
-    Avatar,
-    Heading,
-    Text,
-  } from "@chakra-ui/react";
-import { ArrowLeftEndOnRectangleIcon, ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/solid";
-  
-  export function AvatarAction({ ...props }) {
-    return (
-      <div {...props}>
-        <Menu>
-          <MenuButton
-            aria-label="Options"
-          >
-            <Avatar bg="teal" size="sm" name="Meliora Ho" />
-          </MenuButton>
-          <MenuList>
-            {/* Uncomment and complete this if needed
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Avatar,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
+import {
+  ArrowLeftEndOnRectangleIcon,
+  ArrowLeftStartOnRectangleIcon,
+} from '@heroicons/react/24/solid';
+
+export function AvatarAction({ user, ...props }: { user: User, [key: string]: any }) {
+  return (
+    <div {...props}>
+      <Menu>
+        <MenuButton aria-label="Options">
+          <Avatar bg="teal" size="sm" name="Meliora Ho" />
+        </MenuButton>
+        <MenuList>
+          {/* Uncomment and complete this if needed
             <MenuItem
               fontSize="sm"
               fontWeight="normal"
@@ -28,21 +30,24 @@ import { ArrowLeftEndOnRectangleIcon, ArrowLeftStartOnRectangleIcon } from "@her
               Rename
             </MenuItem>
             */}
-            <div className="flex flex-col px-3 py-2">
-                <Heading size="sm">Meliora Ho</Heading>
-                <Text fontSize="xs" color="gray.600" fontWeight="normal">meliorah@gmail.com</Text>
-            </div>
-            <MenuItem
-              fontSize="sm"
-              fontWeight="normal"
-              color="red"
-              icon={<ArrowLeftStartOnRectangleIcon className="h-4 w-4 text-red" />}
-            >
-              Log out
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </div>
-    );
-  }
-  
+          <div className="flex flex-col px-3 py-2">
+            <Heading size="sm">{user.name}</Heading>
+            <Text fontSize="xs" color="gray.600" fontWeight="normal">
+              {user.email}
+            </Text>
+          </div>
+          <MenuItem
+            fontSize="sm"
+            fontWeight="normal"
+            color="red"
+            icon={
+              <ArrowLeftStartOnRectangleIcon className="h-4 w-4 text-red" />
+            }
+          >
+            Log out
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </div>
+  );
+}
--- components/dashboard/Sidebar/UserProfile/index.tsx
+++ components/dashboard/Sidebar/UserProfile/index.tsx
@@ -10,7 +10,7 @@ export function UserProfile() {
 
   return (
     <div className="p-4 border-b border-gray-200 flex flex-row justify-between items-center">
-      <AvatarAction />
+      <AvatarAction user={user} />
       {user ? <Heading size="sm">Hi, {user?.name}</Heading> : <Spinner />}
     </div>
   );
