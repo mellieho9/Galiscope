08304bc47c7ad5697631a1f73b7df494aa001d53
nhanaa <nhanpro7d@gmail.com>
2024-04-18 01:20:01 -0500

    Connect folder view with backend

---
+++ components/dashboard/FolderView/FolderHeading.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import {
+  CircularProgress,
+  CircularProgressLabel,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
+import { ProgressLabel } from './ProgressLabel';
+import { useGetFolderById } from '@/hooks/folder.hooks';
+import { useGetDocumentsByFolderId } from '@/hooks/document.hooks';
+import CustomButton from '@/components/Button';
+import { useRouter } from 'next/navigation';
+import { useMemo } from 'react';
+
+interface HeadingProps {
+  folderId: string;
+}
+
+export const FolderHeading: React.FC<HeadingProps> = ({
+  folderId,
+}) => {
+  const router = useRouter();
+  const { data: folder } = useGetFolderById(folderId);
+  const { data: documents = [] } = useGetDocumentsByFolderId(
+    folderId
+  );
+
+  if (!folder) {
+    return (
+      <div className="flex flex-col items-bottom justify-start px-6 pt-10 pb-5 border-b border-gray-200">
+        <Heading>Folder Not Found</Heading>
+        <Text fontSize="lg" mb={8}>
+          Oops! The folder you are looking for does not exist.
+        </Text>
+        <CustomButton onClick={() => router.push('/dashboard')}>
+          Go Home
+        </CustomButton>
+      </div>
+    );
+  }
+
+  const incompleteRead = useMemo(() => {
+    return documents.filter(
+      (doc: { status: string }) => doc.status === 'unread'
+    ).length;
+  }, [documents]);
+
+  const completeRead = useMemo(() => {
+    return documents.filter(
+      (doc: { status: string }) => doc.status === 'read'
+    ).length;
+  }, [documents]);
+
+  const progress = useMemo(() => {
+    return (completeRead / (incompleteRead + completeRead)) * 100;
+  }, [incompleteRead, completeRead]);
+
+  return (
+    <div className="w-full flex flex-col items-bottom justify-start px-6 pt-10 pb-5 border-b border-gray-200">
+      <h1 className="text-xl font-bold">{folder.name}</h1>
+      <div className="flex px-2 py-4 flex-row justify-between items-center text-gray-800">
+        <div className="flex flex-row items-center space-x-4">
+          <CircularProgress
+            fontWeight="semibold"
+            color="teal"
+            value={progress}
+          >
+            <CircularProgressLabel>
+              {progress.toFixed(1)}%
+            </CircularProgressLabel>
+          </CircularProgress>
+          <p className="text-sm font-normal">Completion</p>
+        </div>
+        <div className="flex flex-row space-x-4">
+          <ProgressLabel label="to read" metric={incompleteRead} />
+          <ProgressLabel label="completed" metric={completeRead} />
+        </div>
+      </div>
+    </div>
+  );
+};
