25c6b199bd8c4c171e9e240b488324a49c34633c
Meliora Ho <nho10@fordham.edu>
2024-04-18 22:57:39 +0000

    Added onClick

--- components/dashboard/Home/ShortcutGroup.tsx
+++ components/dashboard/Home/ShortcutGroup.tsx
@@ -1,15 +1,23 @@
+"use client";
 import { Heading } from "@chakra-ui/react";
 import { ShortcutButton } from "./ShortcutButton";
 import PaperUpload from "@/components/modals/PaperUpload";
+import { useState } from "react";
 
 export function ShortcutGroup() {
+  const [showUpload, setShowUpload] = useState(false);
+  const handleClick = () => {
+    setShowUpload(true);
+  };
+
   return (
     <div className="w-full flex flex-col items-center justify-center p-20 border-b border-gray-200">
       <Heading size="md">What do you want to do today?</Heading>
       <div className="flex flex-row mt-3 space-x-2">
-        <ShortcutButton onClick={() => <PaperUpload />}>
-          Read a paper
-        </ShortcutButton>
+        <div onClick={handleClick}>
+          <ShortcutButton>Read a paper</ShortcutButton>
+        </div>
+        {showUpload && <PaperUpload />}
         <ShortcutButton>Bookmark a paper</ShortcutButton>
         <ShortcutButton>Refresh on previous findings</ShortcutButton>
       </div>
