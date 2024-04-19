4235b645762169dc3204c2da80efd75f47e75357
Meliora Ho <melioraho9@gmail.com>
2024-04-19 16:47:55 +0000

    Fixed not found screen

--- components/dashboard/FolderView/FolderHeading.tsx
+++ components/dashboard/FolderView/FolderHeading.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   CircularProgress,
@@ -6,39 +6,35 @@ import {
   Heading,
   Spinner,
   Text,
-} from '@chakra-ui/react';
-import { ProgressLabel } from './ProgressLabel';
-import { useGetFolderById } from '@/hooks/folder.hooks';
-import { useGetDocumentsByFolderId } from '@/hooks/document.hooks';
-import CustomButton from '@/components/Button';
-import { useRouter } from 'next/navigation';
-import { useMemo } from 'react';
+} from "@chakra-ui/react";
+import { ProgressLabel } from "./ProgressLabel";
+import { useGetFolderById } from "@/hooks/folder.hooks";
+import { useGetDocumentsByFolderId } from "@/hooks/document.hooks";
+import CustomButton from "@/components/Button";
+import { useRouter } from "next/navigation";
+import { useMemo } from "react";
 
 interface HeadingProps {
   folderId: string;
 }
 
-export const FolderHeading: React.FC<HeadingProps> = ({
-  folderId,
-}) => {
+export const FolderHeading: React.FC<HeadingProps> = ({ folderId }) => {
   const router = useRouter();
   const { data: folder, isLoading: folderLoading } = useGetFolderById(folderId);
-  const { data: documents = [], isLoading: documentsLoading } = useGetDocumentsByFolderId(
-    folderId
-  );
+  const { data: documents = [], isLoading: documentsLoading } =
+    useGetDocumentsByFolderId(folderId);
 
   const incompleteRead = useMemo(() => {
     if (!documents) return 0;
     return documents.filter(
-      (doc: { status: string }) => doc.status === 'unread'
+      (doc: { status: string }) => doc.status === "unread"
     ).length;
   }, [documents]);
 
   const completeRead = useMemo(() => {
     if (!documents) return 0;
-    return documents.filter(
-      (doc: { status: string }) => doc.status === 'read'
-    ).length;
+    return documents.filter((doc: { status: string }) => doc.status === "read")
+      .length;
   }, [documents]);
 
   const progress = useMemo(() => {
@@ -56,12 +52,12 @@ export const FolderHeading: React.FC<HeadingProps> = ({
 
   if (!folder) {
     return (
-      <div className="flex flex-col items-bottom justify-start px-6 pt-10 pb-5 border-b border-gray-200">
-        <Heading>Folder Not Found</Heading>
-        <Text fontSize="lg" mb={8}>
+      <div className="w-full flex flex-col items-bottom justify-start px-6 pt-10 pb-5 border-b border-gray-200">
+        <h1 className="text-xl font-bold">Folder not found</h1>
+        <Text fontSize="sm" mb={8}>
           Oops! The folder you are looking for does not exist.
         </Text>
-        <CustomButton onClick={() => router.push('/dashboard')}>
+        <CustomButton onClick={() => router.push("/dashboard")}>
           Go Home
         </CustomButton>
       </div>
@@ -73,11 +69,7 @@ export const FolderHeading: React.FC<HeadingProps> = ({
       <h1 className="text-xl font-bold">{folder.name}</h1>
       <div className="flex px-2 py-4 flex-row justify-between items-center text-gray-800">
         <div className="flex flex-row items-center space-x-4">
-          <CircularProgress
-            fontWeight="semibold"
-            color="teal"
-            value={progress}
-          >
+          <CircularProgress fontWeight="semibold" color="teal" value={progress}>
             <CircularProgressLabel>
               {progress.toFixed(1)}%
             </CircularProgressLabel>
