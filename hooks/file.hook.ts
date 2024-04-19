b137f95975f5533fe9777498120322f1cc515afd
nhanaa <nhanpro7d@gmail.com>
2024-04-19 15:05:42 -0500

    Create hooks for uploading files 2

--- hooks/file.hook.ts
+++ hooks/file.hook.ts
@@ -15,3 +15,18 @@ export const useUploadDocumentFile = (
   },
   ...options,
 });
+
+export const useUploadImageFile = (
+  options?: UseMutationOptions<{ filepath: string }, Error, { user_id: string; file: File }>
+) => useMutation({
+  mutationKey: ["upload-image-file"],
+  mutationFn: async ({ user_id, file }: { user_id: string; file: File }) => {
+    const formData = new FormData();
+    formData.append("user_id", user_id);
+    formData.append("files", file);
+
+    const { data } = await api.post<{ filepath: string }>("api/file/upload-image", formData);
+    return data;
+  },
+  ...options,
+});
