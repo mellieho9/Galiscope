55cee88254a694e0beb56d33837f920ce13a1a76
mellieho9 <melioraho9@gmail.com>
2024-04-21 19:17:38 -0400

    Added diagram assets and a dictionary to get their path

--- utils/helpers/helpers.ts
+++ utils/helpers/helpers.ts
@@ -1,6 +1,24 @@
+import classDiagram from "public/diagrams/class diagram.svg"
+import flowChart from "public/diagrams/flow chart.svg"
+import mindMap from "public/diagrams/mind map.svg"
+import quadrantDiagram from "public/diagrams/quadrant diagram.svg"
+import sequenceDiagram from "public/diagrams/sequence diagram.svg"
+import stateDiagram from "public/diagrams/state diagram.svg"
+
 export const truncateString = (str: string, num: number) => {
   if (str.length <= num) {
     return str;
   }
   return str.slice(0, num) + "...";
 }
+
+
+
+export const diagramDictionary = {
+  "classDiagram": classDiagram.src,
+  "flowChart":flowChart.src,
+  "mindMap":mindMap.src,
+  "quadrantDiagram":quadrantDiagram.src,
+  "sequenceDiagram":sequenceDiagram.src,
+  "stateDiagram":stateDiagram.src
+};
