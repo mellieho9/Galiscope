2fcf147742e150d9ed729d58a13061df0e323bf6
mellieho9 <melioraho9@gmail.com>
2024-04-20 21:34:25 -0400

    Added an error page

--- components/dual_view/BlurPaperView.tsx
+++ components/dual_view/BlurPaperView.tsx
@@ -4,27 +4,27 @@ import paper from "../../app/paper.png";
 import { CursorArrowRaysIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
-export function BlurPaperView() { 
+export function BlurPaperView() {
   return (
-    <div className="max-h-screen w-full overflow-y-auto">
-      <div style={{ width: "100%", boxShadow: "none" }}>
-        <Image
-          className="brightness-75 blur-sm bg-cover"
-          src={paper.src}
-          objectFit="cover"
-        />
-        <div className="absolute inset-0 flex items-center justify-center w-1/2">
-          <Box boxShadow={"md"} bg="white" p={6} borderRadius="xl">
-            <CursorArrowRaysIcon
-              className="h-6 w-full my-3 mx-1 text-center"
-              fill="teal"
-            />
-            <Heading color="teal" size="sm" className="mb-5 text-center">
-              Click here to continue reading the paper
-            </Heading>
-          </Box>
+    <div className="min-h-screen w-full overflow-y-auto">
+        <div style={{ width: "100%", boxShadow: "none" }}>
+          <Image
+            className="brightness-75 blur-sm bg-cover"
+            src={paper.src}
+            objectFit="fill"
+          />
+          <div className="absolute inset-0 flex items-center justify-center w-1/2">
+            <Box boxShadow={"md"} bg="white" p={6} borderRadius="xl">
+              <CursorArrowRaysIcon
+                className="h-6 w-full my-3 mx-1 text-center"
+                fill="teal"
+              />
+              <Heading color="teal" size="sm" className="mb-5 text-center">
+                Click here to continue reading the paper
+              </Heading>
+            </Box>
+          </div>
         </div>
-      </div>
     </div>
   );
 }
