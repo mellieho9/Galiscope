44a1a7e267353df1f1f0a15855187b80fa1461df
mellieho9 <melioraho9@gmail.com>
2024-04-20 20:58:25 -0400

    Added redirect links

--- components/dashboard/Sidebar/SidebarItem.tsx
+++ components/dashboard/Sidebar/SidebarItem.tsx
@@ -1,16 +1,24 @@
+"use client";
 import { ReactNode } from "react";
+import { useRouter } from "next/navigation"; // Import useNavigation
 
 interface SidebarItemProps {
   icon: ReactNode;
-  redirectTo?: () => void;
+  redirectTo: string;
   title: string;
 }
 
 export function SidebarItem({ icon, redirectTo, title }: SidebarItemProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(redirectTo);
+  };
+
   return (
     <button
       className="z-5 border-transparent flex flex-row p-1 space-x-3 items-center transition ease-in-out delay-150 hover:bg-baby-lime border-l-2 hover:border-teal active:border-teal active:bg-baby-lime cursor-pointer"
-      onClick={redirectTo}
+      onClick={handleClick}
       tabIndex={0}
     >
       <span className="w-5 h-5 text-teal">{icon}</span>
--- components/dashboard/Sidebar/index.tsx
+++ components/dashboard/Sidebar/index.tsx
@@ -1,17 +1,23 @@
-import { FolderMenu } from "./FolderMenu";
+import { ClockIcon, HomeIcon } from "@heroicons/react/24/outline";
 import { SidebarItem } from "./SidebarItem";
 import { UserProfile } from "./UserProfile";
-import { ClockIcon, HomeIcon } from "@heroicons/react/24/outline";
+import { FolderMenu } from "./FolderMenu";
 
 export function Sidebar() {
   return (
     <div className="h-screen w-1/4 border border-gray-200 text-gray-800 font-medium flex flex-col">
       <UserProfile />
       <div className="flex flex-col p-4 space-y-1">
-        <SidebarItem title={"Home"} icon={<HomeIcon />} />
-        <a href="/dashboard/recent">
-          <SidebarItem title={"Recent"} icon={<ClockIcon />} />
-        </a>
+        <SidebarItem
+          redirectTo="/dashboard/"
+          title="Home"
+          icon={<HomeIcon />}
+        />
+        <SidebarItem
+          redirectTo="/dashboard/recent"
+          title="Recent"
+          icon={<ClockIcon />}
+        />
       </div>
       <FolderMenu />
     </div>
