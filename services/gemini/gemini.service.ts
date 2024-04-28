dc599f4a1e6234e56a4b66e21e07ce02a466ca69
Linh T Phan <phanthuylinh221203@gmail.com>
2024-04-28 05:54:55 -0400

    add gemini chat

--- services/gemini/gemini.service.ts
+++ services/gemini/gemini.service.ts
@@ -1,6 +1,7 @@
 import { diagram_history, summary_history } from "@/utils/gemini/finetune";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { sendQuestion } from ".";
+import { GeminiMessage } from '@/types/gemini.types';
 
 export const createTextSummary = async (input: string) => {
   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_TEXT_KEY || '');
@@ -15,3 +16,9 @@ export const createUMLCode = async (input: string) => {
 
   return await sendQuestion({ input, genAI, history })
 };
+
+export const sendGeminiMessage = async ( {input, history}: GeminiMessage ) => {
+  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_UML_KEY || '');
+
+  return await sendQuestion({ input, genAI, history })
+}
