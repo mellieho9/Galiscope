38274a44855a1a9942d3da2750eab94f21f810e5
Linh T Phan <phanthuylinh221203@gmail.com>
2024-04-28 02:53:30 -0400

    Merge branch 'tin-backend-errordetection' of github.com:mellieho9/googleai_hacks into tin-backend-errordetection

--- app/api/gemini/create-diagram/route.ts
+++ app/api/gemini/create-diagram/route.ts
@@ -1,25 +1,35 @@
-import { createTextSummary, createUMLCode } from './../../../../services/gemini/gemini.service';
+import {
+  createTextSummary,
+  createUMLCode,
+} from "./../../../../services/gemini/gemini.service";
 import { NextRequest, NextResponse } from "next/server";
-import axios from 'axios';
+import axios from "axios";
 
 export async function POST(request: NextRequest) {
-  const { diagramType, text } = await request.json()
+  const { diagramType, text } = await request.json();
 
-  const input = `Type: ${diagramType}. Text: ${text}`
+  const input = `Type: ${diagramType}. Text: ${text}`;
 
   // console.log(input)
-  const umlCode = await createUMLCode(input)
+  const umlCode = await createUMLCode(input);
   // console.log('umlCode:' + umlCode)
 
   if (!umlCode) {
-    return NextResponse.json({ error: 'Failed to create diagram' }, { status: 400 });
+    return NextResponse.json(
+      { error: "Failed to create diagram" },
+      { status: 400 }
+    );
   }
 
-  const diagram = await axios.post(process.env.NEXT_PUBLIC_PLANTUML_SERVER_URL || '', { input: umlCode }, { responseType: 'arraybuffer' })
+  const diagram = await axios.post(
+    process.env.NEXT_PUBLIC_PLANTUML_SERVER_URL || "",
+    { input: umlCode },
+    { responseType: "arraybuffer" }
+  );
 
   return new NextResponse(diagram.data, {
     headers: {
-      'Content-Type': 'image/png'
-    }
+      "Content-Type": "image/png",
+    },
   });
 }
