a1908238e480fadab9c71f553dab0a26d047b5ce
Linh T Phan <phanthuylinh221203@gmail.com>
2024-04-28 15:11:06 -0400

    set up gemini bot

--- app/api/gemini/create-diagram/route.ts
+++ app/api/gemini/create-diagram/route.ts
@@ -4,11 +4,12 @@ import {
 } from "./../../../../services/gemini/gemini.service";
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
+import { getTextFromImage } from "@/services/tesseract/tesseract.service";
 
 export async function POST(request: NextRequest) {
   const { diagramType, text } = await request.json();
 
-  const input = `Type: ${diagramType}. Text: ${text}`;
+  let input = `Type: ${diagramType}. Text: ${text}`;
 
   // console.log(input)
   const umlCode = await createUMLCode(input);
@@ -21,11 +22,31 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  const diagram = await axios.post(
+  let diagram = await axios.post(
     process.env.NEXT_PUBLIC_PLANTUML_SERVER_URL || "",
     { input: umlCode },
     { responseType: "arraybuffer" }
   );
+
+  let textImg = ''
+  let textArr: string[] = [];
+  let length = 0
+  let generateTime = 0
+
+  do {
+    textImg = await getTextFromImage(diagram.data)
+    textArr = textImg.split('\n')
+    length = textArr.length
+    generateTime = 0
+  }
+  while (length >= 4 && textArr[length - 2] == "Syntax Error?" && generateTime < 3) {
+    input = `Type: ${diagramType}. Text: ${textImg}`
+    diagram = await axios.post(
+      process.env.NEXT_PUBLIC_PLANTUML_SERVER_URL || "",
+      { input: umlCode },
+      { responseType: "arraybuffer" }
+    );
+  }
 
   return new NextResponse(diagram.data, {
     headers: {
--- components/dual_view/DiagramView/Chatbot/Chatbox.tsx
+++ components/dual_view/DiagramView/Chatbot/Chatbox.tsx
@@ -1,18 +1,27 @@
-import { InputGroup, InputRightElement, Input } from "@chakra-ui/react";
-import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { InputGroup, InputRightElement, Input } from '@chakra-ui/react';
+import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
+import { useRef } from 'react';
 
 export const Chatbox = () => {
+  const msgRef = useRef<string | any>('');
+
+  const sendMessage = () => {
+    const input = msgRef?.current?.value
+    // get diagram schema
+  }
+
   return (
     <div className="w-full pr-8 justify-center absolute bottom-5 z-50">
       <InputGroup size="md" className="flex-row bg-white">
         <Input
-          fontSize={"xs"}
+          ref={msgRef}
+          fontSize={'xs'}
           placeholder="Ask me anything..."
           className="border-0 text-xs focus:ring-0"
           focusBorderColor="teal.main"
         />
         <InputRightElement>
-          <button className="text-teal hover:text-gray-500">
+          <button className="text-teal hover:text-gray-500" onClick={() => sendMessage()}>
             <PaperAirplaneIcon className="w-4 h-4" />
           </button>
         </InputRightElement>
