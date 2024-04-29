c34c0f03c951cfcf336701e1b2362cd088cb4247
Linh T Phan <phanthuylinh221203@gmail.com>
2024-04-29 00:34:06 -0400

    chat history service

--- app/api/gemini/send-message/route.ts
+++ app/api/gemini/send-message/route.ts
@@ -14,27 +14,7 @@ export async function POST(request: NextRequest) {
       { status: 400 }
     );
   }
-
   console.log(geminiResponse)
-
-  history.append(
-    {
-      role: "user",
-      parts: [
-        {
-          text: input
-        }
-      ]
-    },
-    {
-      role: "model",
-      parts: [
-        {
-          text: geminiResponse
-        }
-      ]
-    }
-  )
 
   return NextResponse.json({geminiResponse: geminiResponse, history: history}, { status: 201 });
 }
