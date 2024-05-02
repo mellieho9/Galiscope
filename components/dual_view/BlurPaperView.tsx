96b6af1a1542893b7fae6e6bcebc5601efa66a77
nhanaa <nhanpro7d@gmail.com>
2024-05-01 21:38:53 -0500

    Route blur paper view

--- components/dual_view/BlurPaperView.tsx
+++ components/dual_view/BlurPaperView.tsx
@@ -1,30 +1,43 @@
-"use client";
-import { Box, Heading, Image } from "@chakra-ui/react";
-import paper from "../../public/paper.png"
-import { CursorArrowRaysIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+'use client';
+import { Box, Heading, Image } from '@chakra-ui/react';
+import paper from '../../public/paper.png';
+import { CursorArrowRaysIcon } from '@heroicons/react/24/solid';
+import { useRouter } from 'next/navigation';
 
-export function BlurPaperView() {
+interface BlurPaperViewProps {
+  documentId: string;
+}
+
+export function BlurPaperView({ documentId }: BlurPaperViewProps) {
+  const router = useRouter();
+
   return (
     <div className="min-h-screen w-full overflow-y-auto">
-        <div style={{ width: "100%", boxShadow: "none" }}>
-          <Image
-            className="brightness-75 blur-sm bg-cover w-full"
-            src={paper.src}
-            objectFit="fill"
-          />
-          <div className="absolute inset-0 flex items-center justify-center w-1/2">
-            <Box boxShadow={"md"} bg="white" p={6} borderRadius="xl">
-              <CursorArrowRaysIcon
-                className="h-6 w-full my-3 mx-1 text-center"
-                fill="teal"
-              />
-              <Heading color="teal" size="sm" className="mb-5 text-center">
-                Click here to continue reading the paper
-              </Heading>
-            </Box>
-          </div>
+      <div
+        style={{ width: '100%', boxShadow: 'none' }}
+        onClick={() => router.push(`/pdfViewer/${documentId}`)}
+      >
+        <Image
+          className="brightness-75 blur-sm bg-cover w-full"
+          src={paper.src}
+          objectFit="fill"
+        />
+        <div className="absolute inset-0 flex items-center justify-center w-1/2">
+          <Box boxShadow={'md'} bg="white" p={6} borderRadius="xl">
+            <CursorArrowRaysIcon
+              className="h-6 w-full my-3 mx-1 text-center"
+              fill="teal"
+            />
+            <Heading
+              color="teal"
+              size="sm"
+              className="mb-5 text-center"
+            >
+              Click here to continue reading the paper
+            </Heading>
+          </Box>
         </div>
+      </div>
     </div>
   );
 }
