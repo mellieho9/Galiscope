6a7147631c50444f9aa18178f20ed5ad02ea1114
nhanaa <nhanpro7d@gmail.com>
2024-05-01 21:41:07 -0500

    Change back buttons

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -6,16 +6,11 @@ import { useRouter } from 'next/navigation';
 export const BackButton = ({ ...props }) => {
   const router = useRouter();
 
-  const handleClick = () => {
-    console.log('Back button clicked');
-    router.back();
-  };
-
   return (
     <button
       {...props}
       className="p-2 px-3 shadow rounded-full items-center bg-teal text-white text-sm font-semibold flex flex-row shadow hover:bg-white border-teal hover:border-teal hover:text-teal"
-      onClick={handleClick}
+      onClick={() => router.back()}
     >
       <ArrowLeftIcon className="w-4 h-4 mr-2" />
       Back
--- components/pdfViewer/BackButton.tsx
+++ components/pdfViewer/BackButton.tsx
@@ -1,10 +1,13 @@
-import { ArrowLeftIcon } from "@heroicons/react/24/solid"
+import { ArrowLeftIcon } from '@heroicons/react/24/solid';
+import { useRouter } from 'next/navigation';
 
 export const BackButton = () => {
-    return (
-        <button className="flex hover:opacity-75 items-center">
-          <ArrowLeftIcon className="w-4 h-4 mr-2" />
-          <span className="text-xs">Back</span>
-        </button>
-    )
-}
+  const router = useRouter();
+
+  return (
+    <button className="flex hover:opacity-75 items-center" onClick={() => router.back()}>
+      <ArrowLeftIcon className="w-4 h-4 mr-2" />
+      <span className="text-xs">Back</span>
+    </button>
+  );
+};
